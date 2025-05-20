47a8632c9d76505778101f45b13112b25e8ea338
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 19:57:27 -0400

    Add SceneEventType enumeration for defining various scene events, enhancing event management capabilities.

---
+++ SceneEventType.lua
@@ -0,0 +1,19 @@
+---@meta
+
+---@enum SceneEventType
+SceneEventType = {
+    None = 0,
+    Debug = 1,
+    SpellCancel = 2,
+    SpellStart = 3,
+    SpellComplete = 4,
+    EditorPlay = 5,
+    EditorPause = 6,
+    EditorActorAdd = 7,
+    EditorActorRemove = 8,
+    EditorKeyAdd = 9,
+    EditorKeyRemove = 10,
+    EditorKeyChange = 11,
+    EditorActorVisibilityEnable = 12,
+    EditorActorVisibilityDisable = 13
+}
