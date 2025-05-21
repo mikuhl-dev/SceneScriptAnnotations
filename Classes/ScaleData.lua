029fb370a4b5c37980053fb9cf5f11948261497d
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 21:30:21 -0400

    Add ScaleData class to define scale properties, enhancing scale management capabilities.

---
+++ Classes/ScaleData.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---Does not actually have a constructor currently.
+---@class ScaleData
+---@field scale number
+---@field scaleDuration number
+ScaleData = {}
+ScaleData.__meta = { __index = ScaleData }
