6691ef2be3f40d57cecf08ecda75a808ecc29ef9
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 21:41:15 -0400

    Add WorldText enumeration to define various text types for game events, enhancing text management capabilities.

---
+++ Enums/WorldText.lua
@@ -0,0 +1,18 @@
+---@meta
+
+---@enum WorldText
+WorldText = {
+    Damage          = 0,
+    WhiteDamage     = 1,
+    Absorb          = 5,
+    Critical        = 7,
+    Miss            = 8,
+    XPGain          = 9,
+    HonorGain       = 10,
+    Healing         = 12,
+    HealingCritical = 13,
+    BuildingDamage  = 14,
+    BuildingHeal    = 15,
+    Threat          = 16,
+    None            = 18
+}
