f09990d780ad4f711c8c1149db310b3cc6c21eb6
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 20:36:15 -0400

    Add AnimKits enumeration to define various animation kits, enhancing animation management capabilities.

---
+++ AnimKit.lua
@@ -0,0 +1,13 @@
+---@meta
+
+---@enum AnimKit
+AnimKits = {
+    OneShotSplitBody     = 2127,
+    OneShotUpperBody     = 2128,
+    OneShotFullBody      = 2129,
+    OneShotFullBodyLow   = 2813,
+    LoopingSplitBody     = 2130,
+    LoopingUpperBody     = 2131,
+    LoopingFullBody      = 2132,
+    LoopingSplitBodyRide = 4039,
+}
