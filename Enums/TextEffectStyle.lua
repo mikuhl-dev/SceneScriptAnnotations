841115a107a92a357bf2110e6785e12cc301581c
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 21:50:14 -0400

    Add TextEffectStyle enumeration to define various text effect styles, enhancing text styling management capabilities.

---
+++ Enums/TextEffectStyle.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@enum TextEffectStyle
+TextEffectStyle = {
+    Garrison       = 6,
+    Metal          = 7,
+    Parchment      = 8,
+    IronHordeMetal = 9,
+    BlueGradient   = 10,
+    Legion         = 11,
+}
