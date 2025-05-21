0f824ef7b64838af2c7c05a0e5686bd33e2aaa99
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 21:47:26 -0400

    Add MissReasons enumeration to define various miss reasons, enhancing combat mechanics management capabilities.

---
+++ Enums/MissReasons.lua
@@ -0,0 +1,16 @@
+---@meta
+
+---@enum MissReasons
+MissReasons = {
+    None      = 0,
+    Physical  = 1,
+    Resist    = 2,
+    Dodged    = 3,
+    Parried   = 4,
+    Blocked   = 5,
+    Evaded    = 6,
+    Immune    = 8,
+    Deflected = 9,
+    Absorbed  = 10,
+    Reflected = 11
+}
