7a6964e517a1be3112d90df2b329858cb979deb2
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 21:52:24 -0400

    Add PowerType enumeration to define various power types for character abilities, enhancing power management capabilities.

---
+++ Enums/PowerType.lua
@@ -0,0 +1,23 @@
+---@meta
+
+---@enum PowerType
+PowerType = {
+    Mana          = 0,
+    Rage          = 1,
+    Focus         = 2,
+    Energy        = 3,
+    ComboPoints   = 4,
+    Runes         = 5,
+    RunicPower    = 6,
+    SoulShards    = 7,
+    LunarPower    = 8,
+    HolyPower     = 9,
+    Alternate     = 10,
+    Maelstrom     = 11,
+    Chi           = 12,
+    Insanity      = 13,
+    OBSOLETE      = 15,
+    ArcaneCharges = 16,
+    Fury          = 17,
+    Pain          = 18
+}
