25b819a70f8be32158d20d9f32194a8960ced907
michael <5238952+mikuhl-dev@users.noreply.github.com>
2025-05-20 22:07:09 -0400

    Add ActorPlayerMount class to define properties for player mounts, enhancing mount management capabilities.

---
+++ Classes/ActorPlayerMount.lua
@@ -0,0 +1,9 @@
+---@meta
+
+---@class ActorPlayerMount
+---@field dragonRidingMount boolean
+ActorPlayerMount = {}
+ActorPlayerMount.__meta = { __index = ActorPlayerMount }
+
+---@return ActorPlayerMount
+function ActorPlayerMount:Default() end
